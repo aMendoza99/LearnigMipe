a33c9b51042a5b065a848b8cbd6711515f5951fd
Adrien J. Mendoza Cabrera <amendoza@intelutions.net>
2024-06-12 16:49:37 -0400

    ajustes y tarea #5

---
+++ Tareas/Tarea3/tarea#3.cs
@@ -0,0 +1,30 @@
+﻿//class Program
+//    {
+//        static void Main (string[] Arrays)
+//        {
+//            // Arreglo edades
+//            int[] edades = { 24, 25, 30, 20, 22 };
+
+//            // Arreglo Nombres
+//            string[] nombres = { "Adrien", "Julian", "Matillas", "Ennio", "Antony" };
+
+//            // Logica que presenta Nombre y edad que tiene
+//            for (int i = 0; i < nombres.Length; i++)
+//            {
+//                Console.WriteLine($"{nombres[i]} tiene {edades[i]} años");
+//            }
+
+//            // llamado que presenta Saludo personalizado
+//            for (int i = 0; i < nombres.Length; i++)
+//            {
+//                string mensaje = SaludoPersonalizado(nombres[i], edades[i]);
+//                Console.WriteLine(mensaje);
+//            }
+//        }
+
+//        // Funcion que genera mmensaje personalizado
+//        static string SaludoPersonalizado(string nombre, int edad)
+//        {
+//            return $"Hola {nombre}, tienes {edad} años. ¡Que tengas un buen día!";
+//        }
+//    }
