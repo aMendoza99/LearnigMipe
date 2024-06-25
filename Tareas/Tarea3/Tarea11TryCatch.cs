f9514e347a027669169a6236a582448a63954f8e
Adrien J. Mendoza Cabrera <amendoza@intelutions.net>
2024-06-25 07:49:00 -0400

    tarea #11 try and cathc

---
+++ Tareas/Tarea3/Tarea11TryCatch.cs
@@ -0,0 +1,53 @@
+﻿public class NegativeNumberException : Exception
+{
+    public NegativeNumberException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // ingresar numeros
+            Console.Write("Ingrese el primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            // si es negativo  tirar msj
+            if (num1 < 0 || num2 < 0)
+            {
+                throw new NegativeNumberException("Uno o ambos números son negativos.");
+            }
+
+            // div
+            double result = num1 / num2;
+            Console.WriteLine($"El resultado de la división es: {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: No se puede dividir por cero.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Formato de número no válido.");
+        }
+        catch (NegativeNumberException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error inesperado: {ex.Message}");
+        }
+        finally
+        {
+            // Mensaje
+            Console.WriteLine("La ejecución del bloque try-catch ha finalizado.");
+        }
+    }
+}
--- Tareas/Tarea3/Tarea9LibrosNumeros.cs
+++ Tareas/Tarea3/Tarea9LibrosNumeros.cs
@@ -1,55 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
-class Program
-{
-    static void Main()
-    {
-        //programa para listado de libors/autores
+//class Program
+//{
+//    static void Main()
+//    {
+//        //programa para listado de libors/autores
 
-        var books = new List<dynamic>
-        {
-            new { Title = "The Hobbit", Author = "J.R.R" },
-            new { Title = "Harry Potter and The Chamber of Secrets", Author = "J.K Rowling" },
-            new { Title = "The Art of War", Author = "Sun Tzu" }
-        };
+//        var books = new List<dynamic>
+//        {
+//            new { Title = "The Hobbit", Author = "J.R.R" },
+//            new { Title = "Harry Potter and The Chamber of Secrets", Author = "J.K Rowling" },
+//            new { Title = "The Art of War", Author = "Sun Tzu" }
+//        };
 
-        Console.WriteLine("Lista de Libros:");
-        foreach (var book in books)
-        {
-            Console.WriteLine($"Title: {book.Title}, Author: {book.Author}");
-        }
+//        Console.WriteLine("Lista de Libros:");
+//        foreach (var book in books)
+//        {
+//            Console.WriteLine($"Title: {book.Title}, Author: {book.Author}");
+//        }
 
-        var numbers = new List<int>();
+//        var numbers = new List<int>();
 
-        //Programa de listado de numeros 
+//        //Programa de listado de numeros 
 
-        numbers.Add(10);
-        numbers.Add(20);
-        numbers.Add(30);
-        numbers.Add(40);
-        numbers.Add(50);
+//        numbers.Add(10);
+//        numbers.Add(20);
+//        numbers.Add(30);
+//        numbers.Add(40);
+//        numbers.Add(50);
 
-        numbers.RemoveAt(2);
+//        numbers.RemoveAt(2);
 
-        numbers.Insert(1, 25);
+//        numbers.Insert(1, 25);
 
-        int searchNumber = 40;
-        int index = numbers.IndexOf(searchNumber);
+//        int searchNumber = 40;
+//        int index = numbers.IndexOf(searchNumber);
 
-        Console.WriteLine("\nLista de Números:");
-        foreach (var number in numbers)
-        {
-            Console.WriteLine(number);
-        }
+//        Console.WriteLine("\nLista de Números:");
+//        foreach (var number in numbers)
+//        {
+//            Console.WriteLine(number);
+//        }
 
-        if (index != -1)
-        {
-            Console.WriteLine($"\nEl número {searchNumber} se encuentra en la posición {index}.");
-        }
-        else
-        {
-            Console.WriteLine($"\nEl número {searchNumber} no se encuentra en la lista.");
-        }
-    }
-}
+//        if (index != -1)
+//        {
+//            Console.WriteLine($"\nEl número {searchNumber} se encuentra en la posición {index}.");
+//        }
+//        else
+//        {
+//            Console.WriteLine($"\nEl número {searchNumber} no se encuentra en la lista.");
+//        }
+//    }
+//}
